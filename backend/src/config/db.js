e76f3513b4e7b9beb6d2da092701570e4f499e13
Mohd Arsalan <thesiddiqui7@gmail.com>
2025-04-03 09:26:34 +0530

    update database connection to use environment variable for MongoDB URI

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/auth0');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
