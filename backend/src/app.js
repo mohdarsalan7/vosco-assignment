ab9983968ab5c86cd68b8a24c96df255056a9178
Mohd Arsalan <thesiddiqui7@gmail.com>
2025-04-03 09:41:24 +0530

    remove hardcoded CORS headers from app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,13 +20,6 @@ app.use(
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
